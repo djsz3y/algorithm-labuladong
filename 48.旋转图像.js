a0526641c04f9da9644648907d222b5e131612d0
希雅希雅希雅希雅希雅 <294764484@qq.com>
2022-08-08 20:56:28 +0800

    feat(48.旋转图像): 增加逆时针旋转

--- 48.旋转图像.js
+++ 48.旋转图像.js
@@ -31,15 +31,16 @@ const reverse = function (arr) {
   }
 };
 /**
+ * 二维矩阵原地「顺时针」旋转90°。
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-// 二维矩阵原地顺时针旋转90°。
 var rotate = function (matrix) {
   let len = matrix.length;
-  // 先沿左上右下对角线对称二维矩阵
+  // 先沿 左上右下 对角线 镜像对称 二维矩阵
   for (let i = 0; i < len; i++) {
     for (let j = i; j < len; j++) {
+      // swap(matrix[i][j], matrix[j][i]);
       let temp = matrix[i][j];
       matrix[i][j] = matrix[j][i];
       matrix[j][i] = temp;
@@ -51,18 +52,40 @@ var rotate = function (matrix) {
     reverse(row);
   }
 };
-// let matrix = [
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9],
-//   ],
-//   matrix2 = [
-//     [5, 1, 9, 11],
-//     [2, 4, 8, 10],
-//     [13, 3, 6, 7],
-//     [15, 14, 12, 16],
-//   ];
-// rotate(matrix)
-// rotate(matrix2)
-// console.log(matrix, matrix2);
+/**
+ * 二维矩阵原地「逆时针」旋转90°。
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate_niShiZhen = function (matrix) {
+  let len = matrix.length;
+  // 先沿 左下右上 对角线 镜像对称 二维矩阵
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i; j++) {
+      // swap(matrix[i][j], matrix[n-j-1][n-i-1])
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[len - j - 1][len - i - 1];
+      matrix[len - j - 1][len - i - 1] = temp;
+    }
+  }
+  // 反转二维矩阵的每一行
+  for (let key in matrix) {
+    let row = matrix[key];
+    reverse(row); //顺时针、逆时针 同一个 reverse
+  }
+};
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+  matrix2 = [
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ];
+rotate_niShiZhen(matrix);
+rotate(matrix2);
+console.log(matrix, matrix2);
 // @lc code=end
