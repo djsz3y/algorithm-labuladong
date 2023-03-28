5f8990f9cd17187a87124dedabdef1a2933f948a
djsz3y.SnippetsLab.taylor&suyuan.tian <294764484@qq.com>
2023-03-28 20:04:31 +0800

    feat: 26.删除有序数组中的重复项

--- 26.删除有序数组中的重复项.js
+++ 26.删除有序数组中的重复项.js
@@ -5,43 +5,56 @@
  */
 
 // @lc code=start
+/*
+ * 1.读懂题目：
+ * 升序排列数组 nums
+ * 原地删除重复元素
+ * 返回删除后数组新长度
+ * 保持元素相对顺序一致
+ * --------------------------------------
+ * // console.log(3 * Math.pow(10, 4));
+ * let length = nums.length;
+ * if (length <= 3 * Math.pow(10, 4) && length >= 0) {
+ *   nums = Array.from(new Set(nums));
+ *   return nums.length;
+ * }
+ * 看了labuladong的第26题题解，得知 ：
+ * 我之前的做法是：不原地修改的题解。
+ * 现在是原地修改的题解，才是对的。
+ *
+ * 2.写出思路：
+ * 【不是原地修改】：new一个int类型数组-》把去重之后的元素放进新数组-》返回新数组。
+ * 【原地修改】：不允许new新数组-》只能在原数组上操作，返回新长度-》通过返回的长度&原始数组=移除后的元素有哪些。
+ * 【和 27.移除元素 一样的思路】：
+ * 【快慢指针技巧】：快指针探路，慢指针在后，快指针每找到一个不等于val的元素就赋值给slow并让slow前进一步。
+ * 这样保证了nums[0...slow]就是整个数组移除等于val的元素之后的结果。
+ *
+ * 3.代码实现： removeDuplicates
+ *
+ * 4.测试用例：
+ */
 /**
  * @param {number[]} nums
  * @return {number}
  */
-// 升序排列数组nums
-// 原地删除重复元素
-// 返回新长度
-// 元素相对顺序保持一致
-
-
-  // // console.log(3 * Math.pow(10, 4));
-  // let length = nums.length;
-  // if (length <= 3 * Math.pow(10, 4) && length >= 0) {
-  //   nums = Array.from(new Set(nums));
-  //   return nums.length;
-  // }
-  /**
-   * --------------------------------------
-   * 看了labuladong的第26题题解，得知 ：
-   * 我之前的做法是：不原地修改的题解。
-   * 现在是原地修改的题解，才是对的。
-   */
 var removeDuplicates = function (nums) {
   if (nums.length == 0) {
     return 0;
-  }
+  } // 先判长度
   let slow = 0,
-    fast = 0;
+    fast = 0; // 快慢指针
   while (fast < nums.length) {
-    if (nums[fast] != nums[slow]) {
+    // 判断每次快慢指针位置的值：
+    // 不同，让慢+1，快针值给了慢针值 & 之后，让快+1，去找快慢值不同的位置；
+    // 相同，让快+1，去找快慢值不同的位置。
+    if (nums[fast] !== nums[slow]) {
       slow++;
       nums[slow] = nums[fast];
     }
     fast++;
   }
   // console.log(fast,slow)
-  return slow + 1;
+  return slow + 1; // 长度为索引+1
 };
 // let nums = [1, 1, 2];
 // let res = removeDuplicates(nums);
