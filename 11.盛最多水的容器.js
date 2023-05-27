438cc1a5e9f61a4a2ed0a92d631b8b4c9379edcf
秒变上进宝宝 <294764484@qq.com>
2023-05-27 17:01:03 +0800

    feat: [11] 盛最多水的容器

---
+++ 11.盛最多水的容器.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0; // 起始面级
+  let pStart = 0; // 左指针
+  let pEnd = height.length - 1; // 右指针
+  while (pStart !== pEnd) {
+    let hStart = height[pStart]; // 左高度
+    let hEnd = height[pEnd]; // 右高度
+    let minHeight = hStart < hEnd ? hStart : hEnd; // 最小高度
+    let tmpMax = minHeight * (pEnd - pStart); // 面积
+    max = max > tmpMax ? max : tmpMax; // 与之前面积对比，记录最大面积
+    hStart > hEnd ? pEnd-- : pStart++; // 左、右高度，低的向里走。
+  }
+  return max;
+};
+// @lc code=end
