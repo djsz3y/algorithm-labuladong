900abc91680312445d681c33d0d552a888d84e61
秒变上进宝宝 <294764484@qq.com>
2023-05-29 01:15:28 +0800

    feat: [300] 最长递增子序列

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -5,51 +5,25 @@
  */
 
 // @lc code=start
-/*
- * # 1.读懂题目：
- * 
- * 
- * # 2.写出思路：
- * 
- * 
- * # 3.代码实现： lengthOfLIS
- * 
- * # 4.测试用例：
- */
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
-  let top = new Array(nums.length).fill(-1);
-  // 牌堆数初始化为 0
-  let piles = 0;
-  for (let i = 0; i < nums.length; i++) {
-      // 要处理的扑克牌
-      let poker = nums[i];
+var lengthOfLIS = function (nums) {
+  let dp = [];
+  let max = 1;
+  dp[0] = 1;
 
-      /***** 搜索左侧边界的二分查找 *****/
-      let left = 0, right = piles;
-      while (left < right) {
-          let mid = (left + right) / 2;
-          if (top[mid] > poker) {
-              right = mid;
-          } else if (top[mid] < poker) {
-              left = mid + 1;
-          } else {
-              right = mid;
-          }
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = 1;
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[j] + 1, dp[i]);
       }
-      /*********************************/
-      
-      // 没找到合适的牌堆，新建一堆
-      if (left == piles) piles++;
-      // 把这张牌放到牌堆顶
-      top[left] = poker;
+    }
+    max = Math.max(dp[i], max);
   }
-  // 牌堆数就是 LIS 长度
-  return piles;
+
+  return max;
 };
-
 // @lc code=end
-
