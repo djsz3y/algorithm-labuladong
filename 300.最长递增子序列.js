2b8d111405be7cddf2dc697f8b7335e83958f1eb
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 15:46:40 +0800

    fix: [300] 最长递增子序列

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -10,20 +10,20 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  let dp = [];
-  let max = 1;
-  dp[0] = 1;
+  let dp = []
+  dp[0] = 1
+  let max = 1
 
   for (let i = 1; i < nums.length; i++) {
-    dp[i] = 1;
+    dp[i] = 1
     for (let j = 0; j < i; j++) {
       if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[j] + 1, dp[i]);
+        dp[i] = Math.max(dp[j] + 1, dp[i])
       }
     }
-    max = Math.max(dp[i], max);
+    max = Math.max(dp[i], max)
   }
 
-  return max;
-};
+  return max
+}
 // @lc code=end
