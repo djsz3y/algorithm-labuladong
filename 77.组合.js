472af36f9f12527c3c67909ab72ca5d95aa8b836
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 16:06:09 +0800

    fix: [77] 组合

--- 77.组合.js
+++ 77.组合.js
@@ -12,9 +12,10 @@
  */
 var combine = function (n, k) {
   // 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合
+  // 1......n
 
-  const result = [];
-  const path = [];
+  const res = []
+  const path = []
 
   function backtrack(path, startIndex) {
     // 到达树的底部
@@ -26,31 +27,31 @@ var combine = function (n, k) {
       // ——每个组合长度为 k
 
       // 即：path 的长度为 k 。
-      result.push([...path]);
-      return;
+      res.push([...path])
+      return
     }
 
     // 注意 i 从 startIndex 开始递增
     // 注意小于等于 <=
     for (let i = startIndex; i <= n; i++) {
       // 做选择
-      path.push(i);
+      path.push(i)
       // 回溯阶段让 i + 1，实现 从 1 -> n 的遍历：
 
       // i + 1 后，传给的就是新的 回溯 backtrack 的第二个参数 startIndex，
       // 也就在每次回溯的时候：得到 i 从 startIndex(i + 1) 开始遍历的初始点了。
 
       // 符合数学逻辑。
-      backtrack(path, i + 1);
+      backtrack(path, i + 1)
       // 撤销选择
-      path.pop();
+      path.pop()
     }
   }
 
   // 执行回溯
-  backtrack(path, 1);
+  backtrack(path, 1)
 
   // 返回结果
-  return result;
-};
+  return res
+}
 // @lc code=end
