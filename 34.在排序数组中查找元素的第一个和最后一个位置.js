a47578c743d69bddf18879488244b9db4c5d170a
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 15:55:27 +0800

    fix: [34] 在排序数组中查找元素的第一个和最后一个位置

--- 34.在排序数组中查找元素的第一个和最后一个位置.js
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -12,14 +12,14 @@
  */
 var searchRange = function (nums, target) {
   const search = function (fromLow) {
-    let low = 0;
-    let high = nums.length - 1;
+    let low = 0
+    let high = nums.length - 1
     while (low <= high) {
-      let mid = low + ((high - low) >> 1);
+      let mid = low + ((high - low) >> 1)
       if (nums[mid] < target) {
-        low = mid + 1;
+        low = mid + 1
       } else if (nums[mid] > target) {
-        high = mid - 1;
+        high = mid - 1
       } else {
         // 找到 target 的范围区间内的值的时候——
         // 需要根据 true / false ——
@@ -32,22 +32,22 @@ var searchRange = function (nums, target) {
           // 值，在左区间
           if (nums[mid] === nums[mid - 1]) {
             // 左边还有-相同的-目标值
-            high = mid - 1;
+            high = mid - 1
           } else {
-            return mid;
+            return mid
           }
         } else {
           if (nums[mid] === nums[mid + 1]) {
             // 右边还有-相同的-目标值
-            low = mid + 1;
+            low = mid + 1
           } else {
-            return mid;
+            return mid
           }
         }
       }
     }
-    return -1;
-  };
-  return [search(true), search(false)];
-};
+    return -1
+  }
+  return [search(true), search(false)]
+}
 // @lc code=end
