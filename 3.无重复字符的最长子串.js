790a20ffbf77aea66831ca1ee440d4681805218b
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 14:19:44 +0800

    fix: [3] 无重复字符的最长子串

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -10,28 +10,27 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  let pStart = 0;
-  let pEnd = 0;
-  let maxLength = 0;
+  let maxLength = 0
+  let pStart = 0
+  let pEnd = 0
   // 字符串 -> 数组
-  let sArr = s.split("");
-
+  const sArr = s.split('')
   while (pEnd < sArr.length) {
     // 复制一个子串
-    let subArr = sArr.slice(pStart, pEnd);
+    const subArr = sArr.slice(pStart, pEnd)
     // 问：子串是否存在 pEnd 指针位置的值？
     // 记录：尾指针的值相对于子串存在的位置。
-    let index = subArr.findIndex((item) => item === sArr[pEnd]);
+    let index = subArr.findIndex((item) => item === sArr[pEnd])
 
     // 开始指针右移情况：子串内值，遇重复 sArr[pEnd] 值。
-    pStart = pStart + index + 1;
+    pStart = pStart + index + 1
     // 尾指针每次循环都右移
-    pEnd++;
+    pEnd++
 
     // 记录最长子串长度
-    maxLength = pEnd - pStart > maxLength ? pEnd - pStart : maxLength;
+    maxLength = pEnd - pStart > maxLength ? pEnd - pStart : maxLength
   }
 
-  return maxLength;
-};
+  return maxLength
+}
 // @lc code=end
