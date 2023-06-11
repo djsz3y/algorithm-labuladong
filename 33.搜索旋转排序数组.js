d18b50d9942babd16700352bbf3914ee20cf9d4c
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 15:30:22 +0800

    fix: [33] 搜索旋转排序数组

--- 33.搜索旋转排序数组.js
+++ 33.搜索旋转排序数组.js
@@ -21,30 +21,29 @@
  * @return {number}
  */
 var search = function (nums, target) {
-  let low = 0;
-  let high = nums.length - 1;
+  let low = 0
+  let high = nums.length - 1
   while (low <= high) {
-    let mid = low + ((high - low) >> 1);
+    let mid = low + ((high - low) >> 1)
     if (nums[mid] === target) {
-      return mid;
+      return mid
     }
     if (nums[mid] >= nums[high]) {
       // 左边有序
       if (nums[low] <= target && target < nums[mid]) {
-        high = mid - 1;
+        high = mid - 1
       } else {
-        low = mid + 1;
+        low = mid + 1
       }
     } else if (nums[mid] < nums[high]) {
       // 右边有序
       if (nums[mid] < target && target <= nums[high]) {
-        low = mid + 1;
+        low = mid + 1
       } else {
-        high = mid - 1;
+        high = mid - 1
       }
     }
   }
-  return -1;
-};
-
+  return -1
+}
 // @lc code=end
