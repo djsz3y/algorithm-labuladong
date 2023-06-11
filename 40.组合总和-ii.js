3a5500d74b586fa44efaeb6f863acceed8c20375
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 16:39:59 +0800

    fix: [40] 组合总和 II

--- 40.组合总和-ii.js
+++ 40.组合总和-ii.js
@@ -12,35 +12,36 @@
  */
 var combinationSum2 = function (candidates, target) {
   // 注意：必须排序，否则报错！
-  candidates = candidates.sort((a, b) => a - b);
+  candidates = candidates.sort((a, b) => a - b)
 
-  const result = [];
-  const path = [];
+  const res = []
+  const path = []
 
-  function backtrack(startIndex, sum) {
+  function backtrack(sum, startIndex) {
     if (sum === target) {
-      result.push([...path]);
-      return;
+      res.push([...path])
+      return
     }
     for (let i = startIndex; i < candidates.length; i++) {
       // 剪枝
-      if (candidates[i] + sum > target) return;
+      if (candidates[i] + sum > target) return
       // 数组中可能有相同的数据，这个数据会影响最后的结果，有重复。
+      // 因为上面排过序了，这里判断 candidates 的 i 和 i -1 相同否，就可知道是否选择了重复的。
       if (i === startIndex || candidates[i] !== candidates[i - 1]) {
-        path.push(candidates[i]);
+        path.push(candidates[i])
         // 在每次回溯时候：
         // 把当前循环的项 item(=candidates[i]) 与 sum 相加
         // ——从而计算下次回溯的参数：和 sum 。
 
         // !注意 i + 1
-        backtrack(i + 1, candidates[i] + sum);
-        path.pop();
+        backtrack(candidates[i] + sum, i + 1)
+        path.pop()
       }
     }
   }
 
-  backtrack(0, 0);
+  backtrack(0, 0)
 
-  return result;
-};
+  return res
+}
 // @lc code=end
