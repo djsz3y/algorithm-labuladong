b0d253220c63850357e048ccbccc20199d6720c7
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-11 16:21:26 +0800

    fix: [39] 组合总和

--- 39.组合总和.js
+++ 39.组合总和.js
@@ -11,30 +11,35 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
+  /**
+   * 注意：
+   * 排序，28行可用 break 或者 return ；
+   * 不排序，28 行 用 continue ；
+   */
+
   // 注意：必须排序，否则报错！
-  candidates = candidates.sort((a, b) => a - b);
+  // candidates = candidates.sort((a, b) => a - b)
+  const res = []
+  const path = []
 
-  const result = [];
-  const path = [];
-
-  function backtrack(startIndex, sum) {
+  function backtrack(sum, startIndex) {
     if (sum === target) {
-      result.push([...path]);
-      return;
+      res.push([...path])
+      return
     }
     for (let i = startIndex; i < candidates.length; i++) {
-      if (candidates[i] + sum > target) return;
-      path.push(candidates[i]);
+      if (candidates[i] + sum > target) continue
+      path.push(candidates[i])
       // 在每次回溯时候：
       // 把当前循环的项 item(=candidates[i]) 与 sum 相加
       // ——从而计算下次回溯的参数：和 sum 。
-      backtrack(i, candidates[i] + sum);
-      path.pop();
+      backtrack(candidates[i] + sum, i)
+      path.pop()
     }
   }
 
-  backtrack(0, 0);
+  backtrack(0, 0)
 
-  return result;
-};
+  return res
+}
 // @lc code=end
