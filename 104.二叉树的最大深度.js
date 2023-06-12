d643577ac058528ed01d33cf156d54b91d0914bf
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-12 16:32:41 +0800

    fix: [104] 二叉树的最大深度

--- 104.二叉树的最大深度.js
+++ 104.二叉树的最大深度.js
@@ -18,7 +18,7 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-  if (!root) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-};
+  if (!root) return 0
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+}
 // @lc code=end
