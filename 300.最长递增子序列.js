d1eb4789f959fceea8e42b3be8f34354ecc97c67
修仙 <294764484@qq.com>
2023-09-15 22:06:23 +0800

    fix: [300] 最长递增子序列

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -10,9 +10,11 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
+  // 假如我第 i 个值，大于 第 j 个值。
+  // 那么，我的 dp[i] 的值。等于 j 从 0 到 i-1 各个位置的最长上升子序列+1 的最大值。
   let dp = []
-  dp[0] = 1
   let max = 1
+  dp[0] = 1
 
   for (let i = 1; i < nums.length; i++) {
     dp[i] = 1
