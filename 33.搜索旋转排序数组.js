aa736b0959758ecadac5b68216f5d08aae89135f
修仙 <294764484@qq.com>
2023-09-19 17:14:47 +0800

    fix: [33] 搜索旋转排序数组

--- 33.搜索旋转排序数组.js
+++ 33.搜索旋转排序数组.js
@@ -20,23 +20,40 @@
  * @param {number} target
  * @return {number}
  */
+// 旋转后 的数组 nums [4,5,6,7,0,1,2]
+// 目标值 target
+// 存在返回它的下标，否则返回 -1 。
+// 时间复杂度为 O(log n)
+// 输入：nums = [4,5,6,7,0,1,2], target = 0
+// 输出：4
 var search = function (nums, target) {
-  let low = 0
-  let high = nums.length - 1
+  let low = 0,
+    high = nums.length - 1
+
   while (low <= high) {
     let mid = low + ((high - low) >> 1)
+
+    // 如果中值是目标值，返回mid
     if (nums[mid] === target) {
       return mid
     }
+
+    // 1. 先判断左边有序还是右边有序。
+    // 2. 左边有序，就判断目标值target是否在左边范围内；
+    //    在左边，滑动窗口左移；否做右移。
+    // 3. 右边有序，判断目标值target是否在右边范围内；
+    //    在右边，滑动窗口右移；否则左移。
+    // 4. 注意 target 和 nums[mid] 要么大，要么小，不存在 >= 和 <= 情况；
+    //    如果存在之前就已经 return 了。
     if (nums[mid] >= nums[high]) {
-      // 左边有序
+      // 左边有序——[low, mid)
       if (nums[low] <= target && target < nums[mid]) {
         high = mid - 1
       } else {
         low = mid + 1
       }
     } else if (nums[mid] < nums[high]) {
-      // 右边有序
+      // 右边有序——(mid, high]
       if (nums[mid] < target && target <= nums[high]) {
         low = mid + 1
       } else {
@@ -44,6 +61,7 @@ var search = function (nums, target) {
       }
     }
   }
+
   return -1
 }
 // @lc code=end
