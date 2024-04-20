42c391ff89c5d2f4b35dbf3ccca53334ea838cdb
修仙 <294764484@qq.com>
2024-04-20 10:12:57 +0800

    feat: [70] 爬楼梯

---
+++ 70.爬楼梯.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// 爬楼梯
+// n 阶到达楼顶
+// 每次爬 1 或 2 个台阶，多少种不同的方法
+var climbStairs = function (n) {
+  
+}
+// @lc code=end
