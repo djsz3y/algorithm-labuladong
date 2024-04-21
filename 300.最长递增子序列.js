dc03f1ae4513709d6b631f1108c3cbaec5396e51
修仙 <294764484@qq.com>
2024-04-21 15:14:17 +0800

    feat: Day17.动态规划设计：最长递增子序列

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -10,22 +10,23 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  // 假如我第 i 个值，大于 第 j 个值。
-  // 那么，我的 dp[i] 的值。等于 j 从 0 到 i-1 各个位置的最长上升子序列+1 的最大值。
-  let dp = []
-  let max = 1
-  dp[0] = 1
+  // const dp = []
+  // let res = 0
+  // dp[0] = 1
 
-  for (let i = 1; i < nums.length; i++) {
-    dp[i] = 1
+  const dp = Array(nums.length).fill(1)
+  let res = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    // dp[i] = 1
     for (let j = 0; j < i; j++) {
       if (nums[i] > nums[j]) {
         dp[i] = Math.max(dp[j] + 1, dp[i])
       }
     }
-    max = Math.max(dp[i], max)
+    res = Math.max(dp[i], res)
   }
 
-  return max
+  return res
 }
 // @lc code=end
