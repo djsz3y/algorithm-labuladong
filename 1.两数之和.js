2b9e74070b16dfc5eac167fc6783bcdc5789bfac
djsz3y <294764484.qq.com>
2024-12-06 13:57:10 +0800

    feat: 1.两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -4,6 +4,9 @@
  * [1] 两数之和
  */
 
+// @lcpr-template-start
+
+// @lcpr-template-end
 // @lc code=start
 /**
  * @param {number[]} nums
@@ -17,37 +20,26 @@ var twoSum = function (nums, target) {
   // for in loop nums
   // for in loop nums
   // 对比 和为target return
-  // console.log(nums, Math.pow(10, 4), -Math.pow(10, 4),typeof -Math.pow(10, 4));
-  if (
-    nums.length <= Math.pow(10, 4) &&
-    nums.length >= 2 &&
-    target <= Math.pow(10, 9) &&
-    target >= -Math.pow(10, 9)
-  ) {
-    console.log(nums, target);
-    for (let i = 0; i < nums.length; i++) {
-      const element_i = nums[i];
-      console.log(element_i);
-      if (element_i <= Math.pow(10, 9) && element_i >= -Math.pow(10, 9)) {
-        for (let j = 0; j < nums.length; j++) {
-          let element_j;
-          if (i !== j) {
-            element_j = nums[j];
-            if (element_j <= Math.pow(10, 9) && element_j >= -Math.pow(10, 9)) {
-              const sum = element_i + element_j;
-              if (sum === target) {
-                return [i, j];
-                break;
-              }
-            }
-          }
-        }
-      }
+
+  // 方法：循环数据，每次记录 target - 当前值 得数在映射中存在不？
+  // 存在就返回两个index。
+
+  let valToIndex = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let need = target - nums[i]
+    if (valToIndex.has(need)) {
+      return [valToIndex.get(need), i]
     }
+    valToIndex.set(nums[i], i)
   }
-};
-// const res1 = twoSum([2, 7, 11, 15], 9);
-// const res2 = twoSum([3, 2, 4], 6);
-// const res3 = twoSum([3, 3], 6);
-// console.log(res1, res2, res3);
+  return null
+}
+const res1 = twoSum([2, 7, 11, 15], 9)
+const res2 = twoSum([3, 2, 4], 6)
+const res3 = twoSum([3, 3], 6)
+console.log(res1, res2, res3)
 // @lc code=end
+
+// @after-stub-for-debug-begin
+module.exports = twoSum
+// @after-stub-for-debug-end
