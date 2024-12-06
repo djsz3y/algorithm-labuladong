1d6832d704e0297c857a7fe2d53c1291a08748ae
djsz3y <294764484.qq.com>
2024-12-06 15:28:50 +0800

    fix: 83.删除排序链表中的重复元素.js

--- 83.删除排序链表中的重复元素.js
+++ 83.删除排序链表中的重复元素.js
@@ -4,6 +4,9 @@
  * [83] 删除排序链表中的重复元素
  */
 
+// @lcpr-template-start
+
+// @lcpr-template-end
 // @lc code=start
 /*
  * 1.读懂题目：
@@ -17,7 +20,7 @@
  * 【和 26.删除有序数组中的重复项 一样的思路】：
  * 【快慢指针技巧】：快指针探路，慢指针在后，快指针每找到一个不等于val的元素就赋值给slow并让slow前进一步。
  * 这样保证了nums[0...slow]就是整个数组移除等于val的元素之后的结果。
- * 
+ *
  * 和数组去重一样，
  * 唯一区别：把数组赋值操作变成指针操作。
  *
@@ -37,29 +40,28 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function (head) {
-  if (head == null) return null;
-  let slow = head,
-    fast = head;
+  if (head == null) return null
+  let slow = (fast = head)
   while (fast != null) {
-    if (fast.val != slow.val) {
+    if (fast.val !== slow.val) {
       // nums[slow] = nums[fast]
-      slow.next = fast;
+      slow.next = fast
       // slow++
-      slow = slow.next;
+      slow = slow.next
     }
     // fast++
-    fast = fast.next;
+    fast = fast.next
   }
-  // 断开与后面重复元素的连接
-  slow.next = null;
-  return head;
-};
-let head = { val: 1, next: { val: 1, next: { val: 2 } } },
-  head2 = {
-    val: 1,
-    next: { val: 1, next: { val: 2, next: { val: 3, next: { val: 3 } } } },
-  };
-let res = deleteDuplicates(head),
-  res2 = deleteDuplicates(head2);
-console.log(res, res2);
+  // 链表，所以让 slow 断开与后面重复元素的连接
+  slow.next = null
+  return head
+}
+// let head = { val: 1, next: { val: 1, next: { val: 2 } } },
+//   head2 = {
+//     val: 1,
+//     next: { val: 1, next: { val: 2, next: { val: 3, next: { val: 3 } } } },
+//   };
+// let res = deleteDuplicates(head),
+//   res2 = deleteDuplicates(head2);
+// console.log(res, res2);
 // @lc code=end
