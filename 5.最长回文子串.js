d471d07cc0372c2bb602538a3965d9b871f97537
djsz3y <294764484.qq.com>
2024-12-15 12:12:12 +0800

    fix: [5] 最长回文子串

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -14,7 +14,10 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
-  // 找到字符串s中最长的回文子串
+  // "babad"
+  // "bab"
+  // 最长回文子串
+
   const palindrome = function (s, l, r) {
     while (l >= 0 && r < s.length && s[l] === s[r]) {
       l--
@@ -22,7 +25,9 @@ var longestPalindrome = function (s) {
     }
     return s.substring(l + 1, r)
   }
+
   let res = ''
+
   for (let i = 0; i < s.length; i++) {
     let s1 = palindrome(s, i, i)
     let s2 = palindrome(s, i, i + 1)
@@ -30,6 +35,7 @@ var longestPalindrome = function (s) {
     res = res.length > s1.length ? res : s1
     res = res.length > s2.length ? res : s2
   }
+
   return res
 }
 // @lc code=end
