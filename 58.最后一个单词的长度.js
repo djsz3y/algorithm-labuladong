b1e0090f9a66157b1c5b83376664c937a60d2a5c
djsz3y <294764484.qq.com>
2025-01-07 18:43:59 +0800

    feat: [58] 最后一个单词的长度

---
+++ 58.最后一个单词的长度.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ * @lcpr version=20004
+ *
+ * [58] 最后一个单词的长度
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  s = s.trim()
+
+  let length = 0
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] !== ' ') {
+      length++
+    } else if (length > 0) {
+      break
+    }
+  }
+
+  return length
+}
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "Hello World"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "   fly me   to   the moon  "\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "luffy is still joyboy"\n
+// @lcpr case=end
+
+ */
